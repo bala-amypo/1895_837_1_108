f1db677e42d469676faeef5b83f7110f7419e8c8
bala-amypo <bala@amypo.in>
2025-12-19 00:54:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/DynamicPriceRecord.java
@@ -0,0 +1,68 @@
+package com.example.aiml.model;
+
+import java.time.LocalDateTime;
+
+public class DynamicPriceRecord {
+    private Long id;
+    private Long eventId;
+    private Double computedPrice;
+    private String appliedRuleCodes;
+    private LocalDateTime computedAt;
+    
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getEventId() {
+        return eventId;
+    }
+    public void setEventId(Long eventId) {
+        this.eventId = eventId;
+    }
+    public Double getComputedPrice() {
+        return computedPrice;
+    }
+    public void setComputedPrice(Double computedPrice) {
+        this.computedPrice = computedPrice;
+    }
+    public String getAppliedRuleCodes() {
+        return appliedRuleCodes;
+    }
+    public void setAppliedRuleCodes(String appliedRuleCodes) {
+        this.appliedRuleCodes = appliedRuleCodes;
+    }
+    public LocalDateTime getComputedAt() {
+        return computedAt;
+    }
+    public void setComputedAt(LocalDateTime computedAt) {
+        this.computedAt = computedAt;
+    }
+
+    public DynamicPriceRecord(Long id, Long eventId, Double computedPrice, String appliedRuleCodes,
+            LocalDateTime computedAt) {
+        this.id = id;
+        this.eventId = eventId;
+        this.computedPrice = computedPrice;
+        this.appliedRuleCodes = appliedRuleCodes;
+        this.computedAt = computedAt;
+    }
+    
+    public DynamicPriceRecord() {
+    }
+
+
+
+
+
+    
+}
+
+
+
+
+
+
+    
---
+++ src/main/java/com/example/demo/model/EventRecord.java
@@ -0,0 +1,82 @@
+package com.example.aiml.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class EventRecord {
+    private Long id;
+    private String eventCode;
+    private String eventName;
+    private String venue;
+    private LocalDate eventDate;
+    private Double basePrice;
+    private LocalDateTime createdAt;
+    private Boolean active;
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getEventCode() {
+        return eventCode;
+    }
+    public void setEventCode(String eventCode) {
+        this.eventCode = eventCode;
+    }
+    public String getEventName() {
+        return eventName;
+    }
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+    public String getVenue() {
+        return venue;
+    }
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
+    public LocalDate getEventDate() {
+        return eventDate;
+    }
+    public void setEventDate(LocalDate eventDate) {
+        this.eventDate = eventDate;
+    }
+    public Double getBasePrice() {
+        return basePrice;
+    }
+    public void setBasePrice(Double basePrice) {
+        this.basePrice = basePrice;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public Boolean getActive() {
+        return active;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public EventRecord(Long id, String eventCode, String eventName, String venue, LocalDate eventDate, Double basePrice,
+            LocalDateTime createdAt, Boolean active) {
+        this.id = id;
+        this.eventCode = eventCode;
+        this.eventName = eventName;
+        this.venue = venue;
+        this.eventDate = eventDate;
+        this.basePrice = basePrice;
+        this.createdAt = createdAt;
+        this.active = active;
+    }
+    public EventRecord() {
+    }
+    
+    
+}
+
