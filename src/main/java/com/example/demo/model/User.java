59f051d24ead294314f94e08404db72064994865
bala-amypo <bala@amypo.in>
2025-12-19 04:16:24 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,50 +1,83 @@
 package com.example.aiml.model;
 
+import jakarta.persistence.*;
+
 @Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String fullName;
+
+    @Column(unique = true)
     private String email;
+
+    private String name;
     private String password;
     private String role;
+    private Boolean active;
+
+    
+    public User() {
+    }
+
+    public User(Long id, String email, String name,
+                String password, String role, Boolean active) {
+
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.role = role;
+        this.active = active;
+    }
+
+    
     public Long getId() {
         return id;
     }
     public void setId(Long id) {
         this.id = id;
     }
-    public String getFullName() {
-        return fullName;
-    }
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
+
+    
     public String getEmail() {
         return email;
     }
     public void setEmail(String email) {
         this.email = email;
     }
+
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
     public String getPassword() {
         return password;
     }
     public void setPassword(String password) {
         this.password = password;
     }
+
+    
     public String getRole() {
         return role;
     }
     public void setRole(String role) {
         this.role = role;
     }
-    public User(Long id, String fullName, String email, String password, String role) {
-        this.id = id;
-        this.fullName = fullName;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
-    public User() {
-    }
-}
 
+    
+    public Boolean getActive() {
+        return active;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+}
