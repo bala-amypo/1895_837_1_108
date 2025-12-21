a5eccfb7c6f4938273a76125220292ed789a86c7
bala-amypo <bala@amypo.in>
2025-12-21 04:07:12 +0000

    User commit

--- src/main/java/com/example/demo/repository/DynamicPriceRecordRepository.java
+++ src/main/java/com/example/demo/repository/DynamicPriceRecordRepository.java
@@ -2,9 +2,7 @@ package com.example.demo.repository;
 
 import com.example.demo.model.DynamicPriceRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface DynamicPriceRecordRepository
         extends JpaRepository<DynamicPriceRecord, Long> {
 }
--- src/main/java/com/example/demo/repository/EventRecordRepository.java
+++ src/main/java/com/example/demo/repository/EventRecordRepository.java
@@ -2,11 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.model.EventRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface EventRecordRepository
         extends JpaRepository<EventRecord, Long> {
 
