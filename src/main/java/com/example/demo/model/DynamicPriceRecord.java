61fdd337557519d77ff31a5349c05ec4d50ddd29
bala-amypo <bala@amypo.in>
2025-12-21 10:04:24 +0000

    User commit

--- src/main/java/com/example/demo/model/DynamicPriceRecord.java
+++ src/main/java/com/example/demo/model/DynamicPriceRecord.java
@@ -16,14 +16,19 @@ public class DynamicPriceRecord {
 
     private LocalDateTime computedAt;
 
+    // getters & setters
     public Long getId() { return id; }
 
     public Long getEventId() { return eventId; }
     public void setEventId(Long eventId) { this.eventId = eventId; }
 
     public double getComputedPrice() { return computedPrice; }
-    public void setComputedPrice(double computedPrice) { this.computedPrice = computedPrice; }
+    public void setComputedPrice(double computedPrice) {
+        this.computedPrice = computedPrice;
+    }
 
     public LocalDateTime getComputedAt() { return computedAt; }
-    public void setComputedAt(LocalDateTime computedAt) { this.computedAt = computedAt; }
+    public void setComputedAt(LocalDateTime computedAt) {
+        this.computedAt = computedAt;
+    }
 }
