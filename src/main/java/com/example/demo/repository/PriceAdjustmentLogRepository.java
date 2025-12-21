490f72453cb8b3209e858d91d57af2738906afed
bala-amypo <bala@amypo.in>
2025-12-21 04:01:17 +0000

    User commit

--- src/main/java/com/example/demo/repository/PriceAdjustmentLogRepository.java
+++ src/main/java/com/example/demo/repository/PriceAdjustmentLogRepository.java
@@ -2,10 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.model.PriceAdjustmentLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface PriceAdjustmentLogRepository extends JpaRepository<PriceAdjustmentLog, Long> {
+@Repository
+public interface PriceAdjustmentLogRepository
+        extends JpaRepository<PriceAdjustmentLog, Long> {
 
     List<PriceAdjustmentLog> findByEventCode(String eventCode);
 }
