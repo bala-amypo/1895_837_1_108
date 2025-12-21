67fcf60eaddd0100e611ee61bf074775c18cce30
bala-amypo <bala@amypo.in>
2025-12-21 06:34:13 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,13 +1,10 @@
 package com.example.demo.service;
 
 import com.example.demo.model.User;
-import java.util.Optional;
 
 public interface UserService {
 
     User save(User user);
 
-    Optional<User> findByEmail(String email);
-
-    boolean existsByEmail(String email);
+    User getByEmail(String email);   // ✅ ADD THIS
 }
