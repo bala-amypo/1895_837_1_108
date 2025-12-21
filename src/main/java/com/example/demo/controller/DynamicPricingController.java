ba61aa204866ebddd63145c02b3b5ecd6ecd0dba
bala-amypo <bala@amypo.in>
2025-12-21 10:18:19 +0000

    User commit

--- src/main/java/com/example/demo/controller/DynamicPricingController.java
+++ src/main/java/com/example/demo/controller/DynamicPricingController.java
@@ -2,8 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.model.DynamicPriceRecord;
 import com.example.demo.service.DynamicPricingEngineService;
-
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -16,13 +16,18 @@ public class DynamicPricingController {
         this.service = service;
     }
 
-    @PostMapping("/compute/{eventId}")
+    @PostMapping("/{eventId}")
     public DynamicPriceRecord compute(@PathVariable Long eventId) {
-        return service.computePrice(eventId);
+        return service.compute(eventId);
     }
 
-    @GetMapping("/compute/{eventId}")
-    public DynamicPriceRecord compute(@PathVariable Long eventId) {
-    return service.computeDynamicPrice(eventId);
-}
+    @GetMapping
+    public List<DynamicPriceRecord> getAll() {
+        return service.getAllComputedPrices();
+    }
+
+    @GetMapping("/{eventId}")
+    public List<DynamicPriceRecord> history(@PathVariable Long eventId) {
+        return service.getPriceHistory(eventId);
+    }
 }
