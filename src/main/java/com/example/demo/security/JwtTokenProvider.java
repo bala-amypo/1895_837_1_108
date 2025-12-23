41153cf647f52d21a7b513fe3578a9dda0c27162
bala-amypo <bala@amypo.in>
2025-12-23 04:08:46 +0000

    User commit

---
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -0,0 +1,74 @@
+package com.example.demo.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtTokenProvider {
+
+    private final Key key;
+    private final long validityInMs;
+    private final boolean enabled;
+
+    public JwtTokenProvider(String secret, long validityInMs, boolean enabled) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+        this.validityInMs = validityInMs;
+        this.enabled = enabled;
+    }
+
+    public String generateToken(
+            Authentication authentication,
+            Long userId,
+            String role) {
+
+        String email = authentication.getName();
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("userId", userId);
+        claims.put("role", role);
+        claims.put("email", email);
+
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + validityInMs);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(email)
+                .setIssuedAt(now)
+                .setExpiration(expiry)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            if (!enabled) return false;
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException ex) {
+            return false;
+        }
+    }
+
+    public String getUsernameFromToken(String token) {
+        return getAllClaims(token).getSubject();
+    }
+
+    public Map<String, Object> getAllClaims(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return new HashMap<>(claims);
+    }
+}
