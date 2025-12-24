ada778da5f5b17825f7cc245544aa58dce713d91
bala-amypo <bala@amypo.in>
2025-12-24 13:14:50 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -2,12 +2,18 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     @PostMapping("/token")
-    public String token() {
-        return "Authentication handled internally";
+    public String generateToken(@RequestBody Map<String, String> login) {
+
+        String username = login.get("username");
+        String password = login.get("password");
+
+        return "Token generated successfully for user: " + username;
     }
 }
