099c131a719209f89ec8bfa2b53ef53f734200be
bala-amypo <bala@amypo.in>
2025-12-24 06:06:13 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/PriceAdjustmentLogServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.PriceAdjustmentLog;
+import com.example.demo.repository.PriceAdjustmentLogRepository;
+import com.example.demo.service.PriceAdjustmentLogService;
+import java.util.List;
+
+public class PriceAdjustmentLogServiceImpl implements PriceAdjustmentLogService {
+
+    private final PriceAdjustmentLogRepository repo;
+
+    public PriceAdjustmentLogServiceImpl(PriceAdjustmentLogRepository repo) {
+        this.repo = repo;
+    }
+
+    public List<PriceAdjustmentLog> getAdjustmentsByEvent(Long eventId) {
+        return repo.findByEventId(eventId);
+    }
+}
