ab5c99c3a5e9318bc38da8f9593ad38a6df7df34
bala-amypo <bala@amypo.in>
2025-12-24 06:01:53 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/DynamicPricingEngineService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import com.example.demo.model.*;
+import java.util.List;
+
+public interface DynamicPricingEngineService {
+    DynamicPriceRecord computeDynamicPrice(Long eventId);
+    List<DynamicPriceRecord> getPriceHistory(Long eventId);
+    List<DynamicPriceRecord> getAllComputedPrices();
+}
