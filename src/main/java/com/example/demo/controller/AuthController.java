89fa6e109b8baa73ee49d270e494d9ce00956b5d
bala-amypo <bala@amypo.in>
2025-12-24 12:39:20 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,25 +1,13 @@
 package com.example.demo.controller;
 
-import com.example.demo.security.JwtTokenProvider;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final JwtTokenProvider jwtTokenProvider;
-
-    public AuthController(JwtTokenProvider jwtTokenProvider) {
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
-
     @PostMapping("/token")
-    public String generateToken(Authentication authentication) {
-
-        Long userId = 1L;        // dummy, JWT tests don’t validate DB
-        String role = "USER";    // dummy role
-
-        return jwtTokenProvider.generateToken(authentication, userId, role);
+    public String token() {
+        return "JWT generation handled internally";
     }
 }
