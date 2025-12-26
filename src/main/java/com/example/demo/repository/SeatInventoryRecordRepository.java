59503a601a925a589f737f2b80b36c13188786e5
bala-amypo <bala@amypo.in>
2025-12-26 05:32:32 +0000

    User commit

--- src/main/java/com/example/demo/repository/SeatInventoryRecordRepository.java
+++ src/main/java/com/example/demo/repository/SeatInventoryRecordRepository.java
@@ -1,9 +1,12 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.SeatInventoryRecord;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SeatInventoryRecordRepository extends JpaRepository<SeatInventoryRecord, Long> {
+import java.util.Optional;
+
+public interface SeatInventoryRecordRepository
+        extends JpaRepository<SeatInventoryRecord, Long> {
+
     Optional<SeatInventoryRecord> findByEventId(Long eventId);
 }
