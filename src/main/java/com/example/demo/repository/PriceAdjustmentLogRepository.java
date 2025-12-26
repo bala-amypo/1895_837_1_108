9fdf4abfb1780ae94773c4576e65c8d2aab8f2d9
bala-amypo <bala@amypo.in>
2025-12-26 05:32:03 +0000

    User commit

--- src/main/java/com/example/demo/repository/PriceAdjustmentLogRepository.java
+++ src/main/java/com/example/demo/repository/PriceAdjustmentLogRepository.java
@@ -1,9 +1,12 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.PriceAdjustmentLog;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PriceAdjustmentLogRepository extends JpaRepository<PriceAdjustmentLog, Long> {
+import java.util.List;
+
+public interface PriceAdjustmentLogRepository
+        extends JpaRepository<PriceAdjustmentLog, Long> {
+
     List<PriceAdjustmentLog> findByEventId(Long eventId);
 }
