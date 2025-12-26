d2b37f40b5595350d50028a42622f688933ccb9e
bala-amypo <bala@amypo.in>
2025-12-26 05:32:17 +0000

    User commit

--- src/main/java/com/example/demo/repository/PricingRuleRepository.java
+++ src/main/java/com/example/demo/repository/PricingRuleRepository.java
@@ -1,10 +1,14 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.PricingRule;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PricingRuleRepository extends JpaRepository<PricingRule, Long> {
-    boolean existsByRuleCode(String code);
+import java.util.List;
+
+public interface PricingRuleRepository
+        extends JpaRepository<PricingRule, Long> {
+
+    boolean existsByRuleCode(String ruleCode);
+
     List<PricingRule> findByActiveTrue();
 }
