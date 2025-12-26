2534db1840c84ae4f22b7739403f76a636653cc7
bala-amypo <bala@amypo.in>
2025-12-26 06:26:30 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,14 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.demo.model")
+@EnableJpaRepositories(basePackages = "com.example.demo.repository")
 public class DemoApplication {
 
-	public static void main(String[] args) {
-			SpringApplication.run(DemoApplication.class, args);
-			System.out.println(new BCryptPasswordEncoder().encode("admin123"));
-				}
-				}
-				
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
